db0de015501d195033a662f93b9abffcdd98321b
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-02-09 13:25:51 -0600

    final working app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [data, setData] = useState('loading...');
+  useEffect(() => {
+    fetch('https://api.chucknorris.io/jokes/random')
+      .then((response) => response.json())
+      .then((fetcheddata) => {
+        setData(fetcheddata.value);
+      })
+      .catch((e) => {
+        console.error(`An error occurred: ${e}`);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chuck Norris Jokes</h1>
+        <h2>Listen to this...</h2>
+        <img src={require("./chuck.png")} alt="chuck pic" />
+        <p></p><p></p>
+        <div>
+          <span>{data}</span>
+        </div>
       </header>
     </div>
   );
